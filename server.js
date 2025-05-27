a68f8f0bfa22eaf6c4a0e47382d9c0a233e0a4ad
ibtisamkhizer <ibtisamkhizer7@gmail.com>
2025-05-27 11:13:00 +0500

    comment

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API is running..." + process.env.Environment);
 });
 
 // Routes
